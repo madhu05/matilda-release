2c31828fc821515e7be9365ef94d4ae7e40248bb
“divyagerrit” <divya@matildacloud.com>
2019-03-08 15:53:29 -0600

    Changed path file
    Change-Id: I2a7aaf125583751d0951894faf50a260400380e2

--- matilda_release/test/test_v2_pipeine/Paths.py
+++ matilda_release/test/test_v2_pipeine/Paths.py
@@ -1,22 +1,29 @@
+import subprocess
+
+file_names = ['1_release_management_platform.sql',
+              '2_release_management_operating_system.sql',
+              '3_release_management_project.sql',
+              '4_release_management_application.sql',
+              '5_release_management_frequency.sql',
+              '6_release_management_template.sql',
+              '7_release_management_status_type_cd.sql',
+              '8_release_management_status_cd.sql',
+              '9_release_management_release_type_cd.sql',
+              '10_release_management_vnf_node_type.sql',
+              '11_release_management_vnf_sites.sql',
+              '12_release_management_env_type_cd.sql',
+              '13_release_management_milestone_status_cd.sql']
+
+
 def path():
-    print('started')
-    scripts = ['matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\1_release_management_platform.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\2_release_management_operating_system.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\3_release_management_project.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\4_release_management_application.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\5_release_management_frequency.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\6_release_management_template.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\7_release_management_status_type_cd.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\8_release_management_status_cd.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\9_release_management_release_type_cd.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\10_release_management_vnf_node_type.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\11_release_management_vnf_sites.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\12_release_management_env_type_cd.sql',
-               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\13_release_management_milestone_status_cd.sql']
-    return scripts
-
-
-
-
-
+    command = 'find'
+    directory = '/var/lib/jenkins/workspace/'
+    flag = 'iname'
+    file_paths = []
+    for file in file_names:
+        args = [command, directory, flag, file]
+        process = subprocess.run(args, stdout=subprocess.PIPE)
+        path = process.stdout.decode().strip('\n')
+        file_paths.append(path)
+    return file_paths
 

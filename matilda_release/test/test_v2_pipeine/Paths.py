2697755e9d5a2cb88a7b422983a7f881bbb490c0
“divyagerrit” <divya@matildacloud.com>
2019-03-12 17:29:12 -0500

    changed branch to dev-cleanup
    Change-Id: I139a032682da88fd1334efc4fb628f06c0c0f64b

--- matilda_release/test/test_v2_pipeine/Paths.py
+++ matilda_release/test/test_v2_pipeine/Paths.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/python
 import subprocess
 
@@ -27,4 +28,27 @@ def path():
         path = process.stdout.decode().strip('\n')
         file_paths.append(path)
     return file_paths
+=======
+def path():
+    print('started')
+    scripts = ['matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\1_release_management_platform.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\2_release_management_operating_system.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\3_release_management_project.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\4_release_management_application.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\5_release_management_frequency.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\6_release_management_template.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\7_release_management_status_type_cd.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\8_release_management_status_cd.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\9_release_management_release_type_cd.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\10_release_management_vnf_node_type.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\11_release_management_vnf_sites.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\12_release_management_env_type_cd.sql',
+               'matilda-release\\matilda_release\\db\\ sql_scripts\\dmls\\13_release_management_milestone_status_cd.sql']
+    return scripts
+
+
+
+
+
+>>>>>>> test
 
